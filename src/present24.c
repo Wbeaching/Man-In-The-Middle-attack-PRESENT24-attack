b9135faf6c50ad98a6a423b92f6767db36efa23a
theophiIe <theomolinatti@gmail.com>
2021-04-26 17:22:16 +0200

    Ajout du code pour letester le déchiffrement

--- src/present24.c
+++ src/present24.c
@@ -4,6 +4,7 @@
 
 #include "types.h"
 #include "encrypt.h"
+#include "decrypt.h"
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -27,7 +28,21 @@ int main(int argc, char **argv) {
         printf("\nCipher: %x%x%x\n", cipher_text[0], cipher_text[1], cipher_text[2]);
 
     } else if (!strcmp(argv[1], "-d")) {
-        printf("Unimplemented!\n");
+        u8 subkeys[11][3];
+
+        u8 key[10] = {
+            0xD1, 0xBD, 0x2D, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00
+        };
+
+        generate_round_keys(key, subkeys);
+
+        u8 cipher_text[3] = { 0x47, 0xA9, 0x29 };
+        printf("Cipher: %x%x%x\n\n", cipher_text[0], cipher_text[1], cipher_text[2]);
+
+        u8 *clear_text = PRESENT24_decrypt(cipher_text, subkeys);
+        printf("\nClear: %x%x%x\n", clear_text[0], clear_text[1], clear_text[2]);
+
     } else {
         return printf("Usage: %s -[e | d]\n", argv[0]), 1;
     }
