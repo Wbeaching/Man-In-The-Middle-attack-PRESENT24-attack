ea266d85cea676a3f215bc71ac04cd6f6e2d8631
theophiIe <theomolinatti@gmail.com>
2021-04-28 14:45:28 +0200

    Ajout fonction valid_key (ne marche pas)

--- src/attack.c
+++ src/attack.c
@@ -88,25 +88,78 @@ void quick_sort(u64 *arr, i64 low, i64 high)
 //     }
 // }
 
-i64 binary_search(u64 *arr, i64 low, i64 high, u64 target) {
+void valid_key(u64 ciphers, u64 clears, u8 clear_text2[3], u8 cipher_text2[3]){
+    printf("test\n");
+    u8 k1[3] = { 0x00, 0x00, 0x00};
+    k1[0] = (ciphers & 0x0000FFFFFF000000) >> 40;
+    k1[1] = (ciphers & 0x0000FFFFFF000000) >> 32;
+    k1[2] = (ciphers & 0x0000FFFFFF000000) >> 24;
+    printf("Fin K1\n");
+    
+    u8 k2[3] = { 0x00, 0x00, 0x00};
+    k2[0] = (clears & 0x0000FFFFFF000000) >> 40;
+    k2[1] = (clears & 0x0000FFFFFF000000) >> 32;
+    k2[2] = (clears & 0x0000FFFFFF000000) >> 24;
+    printf("Fin K2\n");
+    
+    u8 round_key1[11][3];
+    generate_round_keys(k1, round_key1);
+    printf("Fin sous clé K1\n");
+
+    u8 *result1 = PRESENT24_encrypt(clear_text2, round_key1);
+    printf("Fin encrypt 1\n");
+    
+    u8 round_key2[11][3];
+    generate_round_keys(k2, round_key2);
+    printf("Fin sous clé K2\n");
+
+    u8 *result2 = PRESENT24_encrypt(result1, round_key2);
+    printf("Fin encrypt 2\n");
+
+    if ((result2[0] == cipher_text2[0]) && (result2[1] == cipher_text2[1]) && (result2[2] == cipher_text2[2])) {
+        printf("K1: %x%x%x\nK2: %x%x%x\n\n", k1[0], k1[1], k1[2], k2[0], k2[1], k2[2]);
+    }
+
+    printf("Fin recherche supp\n");
+}
+
+i64 binary_search(u64 *arr, i64 low, i64 high, u64 target, u8 clear_text2[3], u8 cipher_text2[3]) {
     if (high >= low) {
         i64 mid = low + (high - low) / 2;
 
         // If the element is present at the middle
         // itself
         if ((arr[mid] & 0x0000000000ffffff) == (target & 0x0000000000ffffff)) {
+            i64 i = 1;
+            while (mid-i >= 0 && ((arr[mid-i] & 0x0000000000ffffff) == (target & 0x0000000000ffffff))) {
+                printf("debut sous recherche 1 : %lld, %lld\n", i, mid-i);
+                //printf("debut sous recherche %lld\n", i);
+                valid_key(target, arr[mid-i], clear_text2, cipher_text2);
+                //printf("fin sous recherche\n");
+                i++;
+            }
+            
+            
+            i64 j = 1;
+            while (mid+j < pow(2, 24) && ((arr[mid+j] & 0x0000000000ffffff) == (target & 0x0000000000ffffff))) {
+                printf("debut sous recherche 2 : %lld, %lld\n", j, mid+j);
+                valid_key(target, arr[mid+j], clear_text2, cipher_text2);
+                // printf("fin sous recherche 2 : %lld\n", j);
+                j++;
+            }
+
             return mid;
         }
 
         // If element is smaller than mid, then
         // it can only be present in left subarray
         if ((arr[mid] & 0x0000000000ffffff) > (target & 0x0000000000ffffff)) {
-            return binary_search(arr, low, mid - 1, target);
+            return binary_search(arr, low, mid - 1, target, clear_text2, cipher_text2);
         }
 
         // Else the element can only be present
         // in right subarray
-        return binary_search(arr, mid + 1, high, target);
+        return binary_search(arr, mid + 1, high, target, clear_text2, cipher_text2);
     }
 
     // We reach here when element is not
@@ -205,8 +258,6 @@ u8 *PRESENT24_attack(u8 clear_text[3], u8 cipher_text[3], u8 clear_text2[3], u8 
             quick_sort(clears, 0, (pow(2, 24) - 1));
             printf("fin tri\n");
             
-            
-
             for (u64 i = 0; i < pow(2, 24); i++) {
                 u8 k1[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
                 u8 k2[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -214,7 +265,7 @@ u8 *PRESENT24_attack(u8 clear_text[3], u8 cipher_text[3], u8 clear_text2[3], u8 
                 u8 round_key1[11][3];
                 u8 round_key2[11][3];
 
-                i64 index = binary_search(clears, 0, pow(2, 24), ciphers[i]);
+                i64 index = binary_search(clears, 0, pow(2, 24), ciphers[i], clear_text2, cipher_text2);
                 
                 if(index != -1) {
                     k1[0] = (ciphers[i] & 0x0000FFFFFF000000) >> 40;
