6a47cbdfbb65cf0790253e7b1eba68ebc9e3ab32
dssgabriel <dss.gabriel@protonmail.com>
2021-04-29 00:55:03 +0200

    Added support for CLI input

--- src/present24.c
+++ src/present24.c
@@ -28,7 +28,8 @@ int main(int argc, char **argv) {
 
         u8 *cipher_text = PRESENT24_encrypt(clear_text, subkeys);
         printf("\nCipher: %x%x%x\n", cipher_text[0], cipher_text[1], cipher_text[2]);
-    } else if (!strcmp(argv[1], "-d")) {
+    }
+    else if (!strcmp(argv[1], "-d")) {
         u8 subkeys[11][3];
 
         u8 key[10] = {
@@ -43,15 +44,45 @@ int main(int argc, char **argv) {
 
         u8 *clear_text = PRESENT24_decrypt(cipher_text, subkeys);
         printf("\nClear: %x%x%x\n", clear_text[0], clear_text[1], clear_text[2]);
-    } else if (!strcmp(argv[1], "-a")) {
-        u8 clear_text1[3] = { 0xce, 0x15, 0x7a };
-        u8 cipher_text1[3] = { 0x0e, 0xd3, 0xf0 };
+    }
+    else if (!strcmp(argv[1], "-a")) {
+        if (argc == 6) {
+            i32 a2 = strtol(argv[2], NULL, 16);
+            i32 a3 = strtol(argv[3], NULL, 16);
+            i32 a4 = strtol(argv[4], NULL, 16);
+            i32 a5 = strtol(argv[5], NULL, 16);
 
-        u8 clear_text2[3] = { 0x41, 0x81, 0xc8 };
-        u8 cipher_text2[3] = { 0x65, 0x0e, 0x1e };
+            u8 m1[3] = {
+                (a2 & 0x00ff0000) >> 16,
+                (a2 & 0x0000ff00) >> 8,
+                (a2 & 0x000000ff)
+            };
 
-        PRESENT24_attack(clear_text1, cipher_text1, clear_text2, cipher_text2);
-    } else {
+            u8 c1[3] = {
+                (a3 & 0x00ff0000) >> 16,
+                (a3 & 0x0000ff00) >> 8,
+                (a3 & 0x000000ff)
+            };
+
+            u8 m2[3] = {
+                (a4 & 0x00ff0000) >> 16,
+                (a4 & 0x0000ff00) >> 8,
+                (a4 & 0x000000ff)
+            };
+
+            u8 c2[3] = {
+                (a5 & 0x00ff0000) >> 16,
+                (a5 & 0x0000ff00) >> 8,
+                (a5 & 0x000000ff)
+            };
+
+            PRESENT24_attack(m1, c1, m2, c2);
+        }
+        else {
+            return printf("Usage: %s -a m1 c1 m2 c2\n", argv[0]), 1;
+        }
+    }
+    else {
         return printf("Usage: %s -[e | d | a]\n", argv[0]), 1;
     }
 
