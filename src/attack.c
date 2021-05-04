067b5c8cfb491adc25d83ee05af19267c1be8db9
dssgabriel <dss.gabriel@protonmail.com>
2021-05-05 01:04:28 +0200

    Tweaked binary search; Updated code accordingly to encryption/decryption changes; Added documentation/comments

--- src/attack.c
+++ src/attack.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <time.h>
 
 #include "common.h"
+#include "err.h"
 #include "encrypt.h"
 #include "decrypt.h"
 #include "attack.h"
@@ -14,33 +16,38 @@ typedef void *(*fn_ptr)(void *arg);
 static inline
 void radix_sort_pass(u64 *src, u64 *dst, size_t n, size_t shift)
 {
-    size_t next_index = 0, index[256] = { 0 };
+    size_t index[256] = { 0 }, next_index = 0, count;
 
+    // Count every occurence of the masked digit using the index array
     for (size_t i = 0; i < n; i++)
     {
-        index[(src[i] >> shift) & 0x00000000000000ff]++;
+        index[(src[i] >> shift) & 0xff]++;
     }
 
+    // Accumulate the value of indices into a counter
     for (size_t i = 0; i < 256; i++)
     {
-        size_t count = index[i];
+        count = index[i];
         index[i] = next_index;
         next_index += count;
     }
 
+    // Place the values back into the destination array using the index array
     for (size_t i = 0; i < n; i++)
     {
-        dst[index[(src[i] >> shift) & 0x00000000000000ff]++] = src[i];
+        dst[index[(src[i] >> shift) & 0xff]++] = src[i];
     }
 }
 
 static inline
 void radix_sort(u64 *arr, u64 *tmp, size_t n)
 {
+    // Perform 3 pass on the array (8 bits evaluated and counted per pass)
     radix_sort_pass(arr, tmp, n, 0 * 8);
     radix_sort_pass(tmp, arr, n, 1 * 8);
     radix_sort_pass(arr, tmp, n, 2 * 8);
 
+    // Put the values of the last pass back into the original array
     for (size_t i = 0; i < n; i++)
     {
         arr[i] = tmp[i];
@@ -50,28 +57,35 @@ void radix_sort(u64 *arr, u64 *tmp, size_t n)
 static
 void valid_key(u64 encrypted, u64 decrypted, u8 m2[3], u8 c2[3])
 {
-    u8 rk1[11][3], rk2[11][3], tmp_m2[3];
-    u8 k1[10], k3[10], k2[10], k4[10];
+    // Declare the needed key registers, round key arrays
+    // and a temporary copy of m2
+    u8  k1[10], k2[10], rk1[11][3], rk2[11][3], tmp_m2[3];
 
+    // Initialize the key registers with the master keys and copy m2 into tmp_m2
     for (u8 i = 0; i < 3; i++)
     {
+        k1[i] = (encrypted & MASK_K64) >> (40 - (i * 8));
+        k2[i] = (decrypted & MASK_K64) >> (40 - (i * 8));
         tmp_m2[i] = m2[i];
-        k1[i] = k3[i] = (encrypted & MASK_K64) >> (40 - (i * 8));
-        k2[i] = k4[i] = (decrypted & MASK_K64) >> (40 - (i * 8));
     }
 
+    // Generate the round keys for the first and second encryption
     generate_round_keys(k1, rk1);
     generate_round_keys(k2, rk2);
 
-    u8 *res1 = PRESENT24_encrypt(tmp_m2, rk1);
-    u8 *res2 = PRESENT24_encrypt(res1, rk2);
+    // Encrypt the copy of m2 with the first round keys
+    PRESENT24_encrypt(tmp_m2, rk1);
+    // Encrypt the result of the first encryption with the second round keys
+    PRESENT24_encrypt(tmp_m2, rk2);
 
-    if ((res2[0] == c2[0]) && (res2[1] == c2[1]) && (res2[2] == c2[2]))
+    // Check if the result of the second encryption matches with c2
+    if ((tmp_m2[0] == c2[0]) && (tmp_m2[1] == c2[1]) && (tmp_m2[2] == c2[2]))
     {
         info();
-        printf("\x1b[32mFound pair!\x1b[0m\n    k1: %02x%02x%02x | k2: %02x%02x%02x\n\n",
-            k3[0], k3[1], k3[2],
-            k4[0], k4[1], k4[2]
+        printf("\x1b[32mFound a valid pair (k1, k2)!\x1b[0m\n    k1: %02x%02x%02x | k2: %02x%02x%02x\n\n",
+            (u8)(0xff & (encrypted >> 40)), (u8)(0xff & (encrypted >> 32)),
+            (u8)(0xff & (encrypted >> 24)), (u8)(0xff & (decrypted >> 40)),
+            (u8)(0xff & (decrypted >> 32)), (u8)(0xff & (decrypted >> 24))
         );
     }
 }
@@ -79,51 +93,74 @@ void valid_key(u64 encrypted, u64 decrypted, u8 m2[3], u8 c2[3])
 static
 i64 binary_search(u64 *dict, i64 lo, i64 hi, u64 target, u8 m2[3], u8 c2[3])
 {
-    if (hi >= lo)
+    // Check that hi index is greater than lo index
+    if (hi > lo)
     {
+        // Compute the middle index
         i64 mid = lo + (hi - lo) / 2;
 
-        if ((dict[mid] & MASK_T64) == (target & MASK_T64))
+        // If the target is equal to dict[mid], a collision is found
+        if ((target & MASK_T64) == (dict[mid] & MASK_T64))
         {
-            i64 i = 1;
-            while (((mid - i) >= lo) && ((dict[mid - i] & MASK_T64) == (target & MASK_T64)))
+            // Check for possible collisions around the found target
+            i64 cur = mid - 1;
+            while ((cur >= lo) && ((target & MASK_T64) == (dict[cur] & MASK_T64)))
             {
-                valid_key(target, dict[mid - i], m2, c2);
-                i++;
+                valid_key(target, dict[cur], m2, c2);
+                cur--;
             }
 
-            i = 1;
-            while (((mid + i) <= hi) && ((dict[mid + i] & MASK_T64) == (target & MASK_T64)))
+            cur = mid + 1;
+            while ((cur <= hi) && ((target & MASK_T64) == (dict[cur] & MASK_T64)))
             {
-                valid_key(target, dict[mid + i], m2, c2);
-                i++;
+                valid_key(target, dict[cur], m2, c2);
+                cur++;
             }
 
+            // Return the found index
             return mid;
         }
-
-        if ((dict[mid] & MASK_T64) > (target & MASK_T64))
+        // If the target is less than dict[mid], call recursively
+        // on the lower part of the array
+        else if ((target & MASK_T64) < (dict[mid] & MASK_T64))
         {
             return binary_search(dict, lo, mid - 1, target, m2, c2);
         }
-
-        return binary_search(dict, mid + 1, hi, target, m2, c2);
+        // Else, the target is in the higher part of the array
+        else
+            {
+            return binary_search(dict, mid + 1, hi, target, m2, c2);
+        }
     }
 
+    // If no match was found, return -1
     return -1;
 }
 
 void *attack_dictionaries(void *arg)
 {
-    attack_t *r = (attack_t *)arg;
+    // Cast the void argument to an attack_t
+    attack_t *atk= (attack_t *)arg;
 
-    for (i64 i = r->start; i < r->end; i++)
+    // Iterate over the decrypted array from the given start position to
+    // the given end
+    // Use binary search to find possible collisions with the encrypted array
+    for (i64 i = atk->start; i < atk->end; i++)
     {
-        i64 index = binary_search(r->sorted, 0, DICT_SIZE - 1, r->unsorted[i], r->m, r->c);
+        i64 index = binary_search(
+                        atk->decrypted,
+                        0,
+                        DICT_SIZE - 1,
+                        atk->encrypted[i],
+                        atk->m,
+                        atk->c
+        );
 
+        // If the binary search returned a valid index, check that
+        // the pair of keys (k1, k2) is valid
         if (index != -1)
         {
-            valid_key(r->unsorted[i], r->sorted[index], r->m, r->c);
+            valid_key(atk->encrypted[i], atk->decrypted[index], atk->m, atk->c);
         }
     }
 
@@ -132,11 +169,15 @@ void *attack_dictionaries(void *arg)
 
 void *generate_dictionaries(void *arg)
 {
+    // Cast the void argument to an attack_t
     dictionary_t *dict = (dictionary_t*)arg;
-    u8 rk[11][3], k_reg[10];
+    // Declarations for the key register and round key array
+    u8 k_reg[10], rk[11][3];
 
+    // Iterate over the arrays from the given start position to the given end
     for (u32 i = dict->start; i < dict->end; i++)
     {
+        // Declare and initialize temporary copies of m and c
         u8 tmp_m[3], tmp_c[3];
         for (u8 i = 0; i < 3; i++)
         {
@@ -144,45 +185,56 @@ void *generate_dictionaries(void *arg)
             tmp_c[i] = dict->c[i];
         }
 
+        // Initialize the key register based on i (master key generation)
         k_reg[0] = (i & 0xff0000) >> 16;
         k_reg[1] = (i & 0x00ff00) >> 8;
         k_reg[2] =  i & 0x0000ff;
 
+        // Place the key into the arrays (bits 48 - 63)
         dict->encrypted[i] |= k_reg[0] << 16 | k_reg[1] << 8 | k_reg[2];
         dict->decrypted[i] |= k_reg[0] << 16 | k_reg[1] << 8 | k_reg[2];
-
-        generate_round_keys(k_reg, rk);
-
-        u8 *res1 = PRESENT24_encrypt(tmp_m, rk);
-        u8 *res2 = PRESENT24_decrypt(tmp_c, rk);
-
+        // Shift the key in the right position in the arrays (bits 24 - 47)
         dict->encrypted[i] <<= 24;
         dict->decrypted[i] <<= 24;
 
-        dict->encrypted[i] |= res1[0] << 16 | res1[1] << 8 | res1[2];
-        dict->decrypted[i] |= res2[0] << 16 | res2[1] << 8 | res2[2];
+        // Generate the round keys for the `i`th master key
+        generate_round_keys(k_reg, rk);
+
+        // Encrypt m using the `i`th round keys
+        PRESENT24_encrypt(tmp_m, rk);
+        // Decrypt c using the `i`th round keys
+        PRESENT24_decrypt(tmp_c, rk);
+
+        // Place the text in the right position in the arrays (bits 48 - 63)
+        dict->encrypted[i] |= tmp_m[0] << 16 | tmp_m[1] << 8 | tmp_m[2];
+        dict->decrypted[i] |= tmp_c[0] << 16 | tmp_c[1] << 8 | tmp_c[2];
     }
 
     return NULL;
 }
 
-void PRESENT24_attack(u8 m1[3], u8 c1[3], u8 m2[3], u8 c2[3], size_t NB_THREADS)
+static inline
+void dictionary_part(
+    u64 *encrypted,
+    u64 *decrypted,
+    u8 m1[3],
+    u8 c1[3],
+    size_t NB_THREADS)
 {
-    // Declarations for measuring time
+    // Declare for measuring time
     struct timespec before, after;
 
-    // Allocations
-    u64 *decrypted = malloc(sizeof(u64) * DICT_SIZE);
-    u64 *encrypted = malloc(sizeof(u64) * DICT_SIZE);
+    // Allocate threads and dictionary structure
     pthread_t *tid1 = malloc(sizeof(pthread_t) * NB_THREADS);
     dictionary_t *dict = malloc(sizeof(dictionary_t) * NB_THREADS);
+
+    // Initialize function pointer for the dictionary generation part
     fn_ptr handle1 = generate_dictionaries;
 
-
-    info(); printf("Attack parallelized with %lu threads\n", NB_THREADS);
-    info(); printf("Generating dictionaries... ");
-    // Parallelized dictionary generation
+    // Start time measurement for the dictionary generation
     clock_gettime(CLOCK_MONOTONIC_RAW, &before);
+
+    // Initialize dictionary structure and start threads
     for (u8 i = 0; i < NB_THREADS; i++)
     {
         dict[i].decrypted = decrypted;
@@ -196,37 +248,76 @@ void PRESENT24_attack(u8 m1[3], u8 c1[3], u8 m2[3], u8 c2[3], size_t NB_THREADS)
         }
         pthread_create(tid1 + i, NULL, handle1, dict + i);
     }
+
+    // Wait for threads to finish with join
     for (u8 i = 0; i < NB_THREADS; i++)
     {
         pthread_join(tid1[i], NULL);
     }
+
+    // Stop time measurement for the dictionary generation
     clock_gettime(CLOCK_MONOTONIC_RAW, &after);
-    measure_time(&before, &after);
+    // Print time taken to terminal
+    printf("done in %.3lf secs\n", measure_time(&before, &after));
+
+    // Free the threads and dictionary structure
     free(dict);
     free(tid1);
+}
+
+static inline
+void sorting_part(u64 *encrypted, u64 *decrypted)
+{
+    // Declare for measuring time
+    struct timespec before, after;
+
+    // Allocate a temporary array for the radix sort
+    u64 *tmp = malloc(sizeof(u64) * DICT_SIZE);
+
+    // Start time measurement for the sorting part
+    clock_gettime(CLOCK_MONOTONIC_RAW, &before);
 
     // Sorting both dictionaries for faster lookup
-    u64 *tmp = malloc(sizeof(u64) * DICT_SIZE);
-    info(); printf("Sorting dictionaries... ");
-    clock_gettime(CLOCK_MONOTONIC_RAW, &before);
     radix_sort(decrypted, tmp, DICT_SIZE);
     radix_sort(encrypted, tmp, DICT_SIZE);
-    clock_gettime(CLOCK_MONOTONIC_RAW, &after);
-    measure_time(&before, &after);
-    free(tmp);
 
-    // Performing attack on dictionaries
+    // Stop time measurement for the sorting part
+    clock_gettime(CLOCK_MONOTONIC_RAW, &after);
+    // Print time taken to terminal
+    printf("done in %.3lf secs\n", measure_time(&before, &after));
+
+    // Free the temporary array
+    free(tmp);
+}
+
+static inline
+void attack_part(
+    u64 *encrypted,
+    u64 *decrypted,
+    u8 m2[3],
+    u8 c2[3],
+    size_t NB_THREADS)
+{
+    // Declare for measuring time
+    struct timespec before, after;
+
+    // Allocate threads and attack structure
     pthread_t *tid2 = malloc(sizeof(pthread_t) * NB_THREADS);
     attack_t *atk = malloc(sizeof(attack_t) * NB_THREADS);
+
+    // Initialize function pointer for attack part
     fn_ptr handle2 = attack_dictionaries;
-    info(); printf("Checking for a valid key pair...\n");
+
+    // Start time measurement for the attack part
     clock_gettime(CLOCK_MONOTONIC_RAW, &before);
+
+    // Initialize attaack structure and start threads
     for (u8 i = 0; i < NB_THREADS; i++)
     {
         atk[i].start = i * (DICT_SIZE / NB_THREADS);
         atk[i].end = (i + 1) * (DICT_SIZE / NB_THREADS);
-        atk[i].sorted = decrypted;
-        atk[i].unsorted = encrypted;
+        atk[i].decrypted = decrypted;
+        atk[i].encrypted = encrypted;
         for (u8 j = 0; j < 3; j++)
         {
             atk[i].m[j] = m2[j];
@@ -234,16 +325,41 @@ void PRESENT24_attack(u8 m1[3], u8 c1[3], u8 m2[3], u8 c2[3], size_t NB_THREADS)
         }
         pthread_create(tid2 + i, NULL, handle2, atk + i);
     }
+
+    // Wait for threads to finish with join
     for (u8 i = 0; i < NB_THREADS; i++)
     {
         pthread_join(tid2[i], NULL);
     }
-    clock_gettime(CLOCK_MONOTONIC_RAW, &after);
-    info(); printf("Attack ");
-    measure_time(&before, &after);
 
+    // Stop time measurement for the attack part
+    clock_gettime(CLOCK_MONOTONIC_RAW, &after);
+    // Print time taken to terminal
+    info(); printf("Attack finished in %.3lf secs\n", measure_time(&before, &after));
+
+    // Free threads and attack structure
     free(tid2);
     free(atk);
+}
+
+void PRESENT24_attack(u8 m1[3], u8 c1[3], u8 m2[3], u8 c2[3], size_t NB_THREADS)
+{
+    info(); printf("Attack parallelized with %lu threads\n", NB_THREADS);
+
+    // Allocate arrays
+    u64 *encrypted = malloc(sizeof(u64) * DICT_SIZE);
+    u64 *decrypted = malloc(sizeof(u64) * DICT_SIZE);
+
+    info(); printf("Generating dictionaries... ");
+    dictionary_part(encrypted, decrypted, m1, c1, NB_THREADS);
+
+    info(); printf("Sorting dictionaries... ");
+    sorting_part(encrypted, decrypted);
+
+    info(); printf("Checking for a valid pair of keys...\n");
+    attack_part(encrypted, decrypted, m2, c2, NB_THREADS);
+
+    // Free arrays
     free(decrypted);
     free(encrypted);
 }
@@ -262,40 +378,44 @@ void main_attack(i32 numb_args, i8 **args)
         if (!strcmp(args[6], "-t"))
         {
             size_t a7 = atoi(args[7]);
-            if (a7 < 1 || a7 > 1025)
-            {
-                warn();
-                printf("Invalid number of threads, running with default (4)\n");
-            }
-            else
+            if (a7 > 0 && a7 < 1025)
             {
                 NB_THREADS = a7;
             }
+            else
+            {
+                warn();
+                printf("%lu is an invalid number of threads, running with 4 by default\n", a7);
+            }
+        }
+        else {
+            warn();
+            printf("Invalid thread option, running with 4 by default\n");
         }
     }
 
     u8 m1[3] = {
-        (a2 & 0x00ff0000) >> 16,
-        (a2 & 0x0000ff00) >> 8,
-        (a2 & 0x000000ff)
+        (a2 & 0xff0000) >> 16,
+        (a2 & 0x00ff00) >> 8,
+        (a2 & 0x0000ff)
     };
 
     u8 c1[3] = {
-        (a3 & 0x00ff0000) >> 16,
-        (a3 & 0x0000ff00) >> 8,
-        (a3 & 0x000000ff)
+        (a3 & 0xff0000) >> 16,
+        (a3 & 0x00ff00) >> 8,
+        (a3 & 0x0000ff)
     };
 
     u8 m2[3] = {
-        (a4 & 0x00ff0000) >> 16,
-        (a4 & 0x0000ff00) >> 8,
-        (a4 & 0x000000ff)
+        (a4 & 0xff0000) >> 16,
+        (a4 & 0x00ff00) >> 8,
+        (a4 & 0x0000ff)
     };
 
     u8 c2[3] = {
-        (a5 & 0x00ff0000) >> 16,
-        (a5 & 0x0000ff00) >> 8,
-        (a5 & 0x000000ff)
+        (a5 & 0xff0000) >> 16,
+        (a5 & 0x00ff00) >> 8,
+        (a5 & 0x0000ff)
     };
 
     printf("\x1b[1mMan In The Middle attack on 2PRESENT24 with:\x1b[0m\n");
@@ -308,6 +428,5 @@ void main_attack(i32 numb_args, i8 **args)
     PRESENT24_attack(m1, c1, m2, c2, NB_THREADS);
     clock_gettime(CLOCK_MONOTONIC_RAW, &after);
 
-    f64 time_taken = (after.tv_sec - before.tv_sec) + (after.tv_nsec - before.tv_nsec) / 1E9;
-    info(); printf("Program run in \x1b[1m%.3lf secs\x1b[0m\n", time_taken);
+    info(); printf("Program run in \x1b[1m%.3lf secs\x1b[0m\n", measure_time(&before, &after));
 }
--- src/attack.h
+++ src/attack.h
@@ -2,23 +2,80 @@
 
 #include "types.h"
 
+/**
+ * A structure that holds the data that a thread needs to perform
+ * the dictionary generation.
+ */
 typedef struct {
+    // The array of texts that will be encrypted
     u64 *encrypted;
+
+    // The array of texts that will be decrypted
     u64 *decrypted;
+
+    // The array's starting index on which the thread will work
     u32 start;
+
+    // The array's ending index on which the thread will work
     u32 end;
+
+    // The plain text that need to be encrypted
     u8 m[3];
+
+    // The cipher text that need to be decrypted
     u8 c[3];
 } dictionary_t;
 
+/**
+ * A structure that holds the data that a thread needs to perform
+ * the attack on the dictionaries.
+ */
 typedef struct {
-    u64 *sorted;
-    u64 *unsorted;
-    i64 start;
-    i64 end;
+    // The array of encrypted texts
+    u64 *encrypted;
+
+    // The array of decrypted texts
+    u64 *decrypted;
+
+    // The array's starting index on which the thread will work
+    u32 start;
+
+    // The array's ending index on which the thread will work
+    u32 end;
+
+    // The plain text that need to be encrypted
     u8 m[3];
+
+    // The cipher text that need to be decrypted
     u8 c[3];
 } attack_t;
 
+/**
+ * Perfoms a Man In The Middle Attack on the 2PRESENT24 block cipher.
+ *
+ * For every possible key, puts the results of the encryption of m1 and
+ * the decryption of c1 in arrays (dictionaries).
+ * Sorts both of these using radix sort.
+ * Iterates over one of the arrays and looks for a matching result in the other
+ * using a recursive binary search. If a collision is found (i.e the binary
+ * search returns a valid index), a check is performed for pair of keys
+ * k1 and k2 (used to encrypt/decrypt the matching texts) with m2 and c2.
+ * The check by encrypting m2. The result is then re-encrypted, if it matches
+ * with c2, then the pair of keys k1 and k2 is valid.
+ *
+ * @param m1 The message to use to generate the encrypt dictionary.
+ * @param c1 The cipher to use to generate the decrypt dictionary.
+ * @param m2 The message used to check that a pair of keys k1 and k2 is valid.
+ * @param c2 The cipher used to check that a pair of keys k1 and k2 is valid.
+ * @param rk NB_THREADS The number of threads to use to parallelize the attack.
+ */
 void PRESENT24_attack(u8 m1[3], u8 c1[3], u8 m2[3], u8 c2[3], size_t NB_THREADS);
-void main_attack(i32 numb_args, i8 **args);
+
+/**
+ * Sets up the needed parameters if the program was started
+ * in attack mode. Avoids having all the code in the main function.
+ *
+ * @param nb_args The number of arguments passed.
+ * @param args The array of passed arguments.
+ */
+void main_attack(i32 nb_args, i8 **args);
