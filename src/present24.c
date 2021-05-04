62dae21015c460b79a81986c3db3f38d9fa20988
dssgabriel <dss.gabriel@protonmail.com>
2021-05-05 01:04:55 +0200

    Added comments

--- src/present24.c
+++ src/present24.c
@@ -6,21 +6,28 @@
 
 #include "types.h"
 #include "common.h"
+#include "err.h"
 #include "encrypt.h"
 #include "decrypt.h"
 #include "attack.h"
 
 int main(int argc, char **argv)
 {
+    // Check that user passes at least 2 arguments
     if (argc < 2)
     {
-        return printf("USAGE: %s -[e | d | a]\n", argv[0]), 1;
+        err(ERR_INVALID_RUN_CMD);
+        printf("USAGE: %s -[e | d | a]\n", argv[0]);
+        return 1;
     }
 
+    // If the passed option is `-e`, start encryption mode
     if (!strcmp(argv[1], "-e"))
     {
+        // Check that the number of argument is correct
         if (argc == 4)
         {
+            // Check that the arguments are valid before executing
             if (check_args(argv[2]) == 0 && check_args(argv[3]) == 0)
             {
                 main_encrypt(argv[2], argv[3]);
@@ -28,13 +35,18 @@ int main(int argc, char **argv)
         }
         else
         {
-            return printf("USAGE: %s -e MESSAGE KEY\n", argv[0]), 1;
+            err(ERR_IVALID_ENCRYPTION_CMD);
+            printf("USAGE: %s -e MESSAGE KEY\n", argv[0]);
+            return 2;
         }
     }
+    // If the passed option is `-d`, start decryption mode
     else if (!strcmp(argv[1], "-d"))
     {
+        // Check that the number of argument is correct
         if (argc == 4)
         {
+            // Check that the arguments are valid before executing
             if (check_args(argv[2]) == 0 && check_args(argv[3]) == 0)
             {
                 main_decrypt(argv[2], argv[3]);
@@ -42,13 +54,18 @@ int main(int argc, char **argv)
         }
         else
         {
-            return printf("USAGE: %s -d CIPHER KEY\n", argv[0]), 1;
+            err(ERR_INVALID_DECRYPTION_CMD);
+            printf("USAGE: %s -d CIPHER KEY\n", argv[0]);
+            return 3;
         }
     }
+    // If the passed option is `-a`, start decryption mode
     else if (!strcmp(argv[1], "-a"))
     {
+        // Check that the number of argument is valid
         if (argc > 5)
         {
+            // Check that the arguments are valid before executing
             if (check_args(argv[2]) == 0 && check_args(argv[3]) == 0 &&
                 check_args(argv[4]) == 0 && check_args(argv[5]) == 0)
             {
@@ -57,12 +74,16 @@ int main(int argc, char **argv)
         }
         else
         {
-            return printf("USAGE: %s -a m1 c1 m2 c2\n", argv[0]), 1;
+            err(ERR_INVALID_ATTACK_CMD);
+            printf("USAGE: %s -a m1 c1 m2 c2\n", argv[0]);
+            return 4;
         }
     }
     else
     {
-        return printf("USAGE: %s -[e | d | a]\n", argv[0]), 1;
+        err(ERR_INVALID_OPTION);
+        printf("USAGE: %s -[e | d | a]\n", argv[0]);
+        return 1;
     }
 
     return 0;
